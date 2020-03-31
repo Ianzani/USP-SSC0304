be1a215cc1c6108a3500795fb036df4181658212
LDFranck <43423861+LDFranck@users.noreply.github.com>
2020-03-31 11:24:48 -0300

    Add files via upload

---
+++ 8_exVetorRand.c
@@ -0,0 +1,32 @@
+// Author: LDFranck - 11845726
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TAM 5
+
+int main(){
+
+	int vetor[TAM] = {0}, iMaior = 0, iSegMaior = 0;
+	
+	srand(time(NULL));
+	
+	for(int i=0; i<TAM; i++){
+		
+		vetor[i] = rand()%20;
+		iMaior = (vetor[iMaior]>vetor[i])?iMaior:i;
+		iSegMaior = (vetor[iMaior]>vetor[iSegMaior] && vetor[iSegMaior]>vetor[i])?iSegMaior:i;
+	
+	}
+	
+	printf("Vetor: ");
+	for(int i=0; i<TAM; i++)printf("%d ", vetor[i]);
+	printf("\n");
+	
+	printf("Maior valor: %d\n", vetor[iMaior]);
+	printf("Segundo maior valor: %d\n", vetor[iSegMaior]);
+	
+	system("pause");
+	
+}
